b3fa147ac13f9badbcb553f781c71f8a7307be5b
annassanchez <annassanchez@gmail.com>
2022-11-10 21:16:41 +0100

    ⊙.☉ creo que he definido una clase

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,24 +1,28 @@
-
 # Soldier
 
-
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    #metodo01: attack, esta función 
+    def attack():
+        return self.strength
+    #metodo02: attack, esta función 
+    def receiveDamage(damage):
+        damage -= self.health
 
 # Viking
-
 
 class Viking:
     pass
 
 # Saxon
 
-
 class Saxon:
     pass
 
 # War
-
 
 class War:
     pass
