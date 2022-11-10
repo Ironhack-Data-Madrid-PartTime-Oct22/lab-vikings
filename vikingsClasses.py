4cf02058810bf1f9e8dc864007dc5728bc8136cd
XiangLinZ <nasder10@hotmail.com>
2022-11-10 21:39:01 +0100

    cero fantasía 🥺🥺

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,13 +3,32 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
 
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, the_damage):
+        self.health -= the_damage
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, health, strength, name):
+        super().__init__(health, strength)
+        self.name = name
+
+    def receiveDamage(self, the_damage):
+        self.health -= the_damage
+        if self.health > 0:
+            return f"{self.name} has received {the_damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
