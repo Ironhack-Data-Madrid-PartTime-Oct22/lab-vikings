af192c7bd1c6aa1ce6e7d4a307875d568ffc7d74
annassanchez <annassanchez@gmail.com>
2022-11-11 20:00:19 +0100

    ___*( ￣皿￣)/#____ el bonus no furula

--- file.py
+++ file.py
@@ -1,29 +1,34 @@
-import vikingsClasses
+from vikingsClasses import Soldier, Viking, Saxon, War
 #import Soldier 
 #mport War 
 import names ##generador aleatorio de nombres porque no me apetece pensar
 import random
-   
+
+war = War()
+
 # creamos el ejercito de vikingos
 v = int(input("number of vikings: "))
-i = 0
-armyViking = []
-while i < v:
-    viking = vikingsClasses.Viking(names.get_first_name(), random.randint(1, 300), random.randint(1, 300))
-    print(viking.__dict__)
-    testViking = vikingsClasses.War()
-    armyViking.append(viking)
+i = 1
+while i <= v:
+    viking = Viking(names.get_first_name(), random.randint(0, 300), random.randint(0, 300))
+    print(f"vikingo numero {i}:", viking.__dict__)
+    war.addViking(viking)
     i +=1
-print(type(testViking), type(armyViking))
 
 # creamos el ejercito de sajones
 s = int(input("number of saxons: "))
-i = 0
-armySaxon = []
-while i < s:
-    saxon = vikingsClasses.Saxon(random.randint(1, 300), random.randint(1, 300))
-    print(saxon.__dict__)
-    testSaxon = vikingsClasses.War()
-    armySaxon.append(saxon)
+i = 1
+while i <= s:
+    saxon = Saxon(random.randint(1, 300), random.randint(1, 300))
+    print(f"sajón numero {i}:", saxon.__dict__)
+    war.addSaxon(saxon)
     i +=1
-print(type(testSaxon), type(armySaxon))
+
+## a pegarse
+rounds = 0
+while rounds < 10:
+    war.vikingAttack()
+    war.saxonAttack()
+    check = war.showStatus()
+    print(check)
+    rounds +=1
