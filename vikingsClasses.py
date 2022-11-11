6d094378a0f3629ff6eb0dc67edcc7e8c76e6100
ppastorcarrillo <116591098+ppastorcarrillo@users.noreply.github.com>
2022-11-11 21:51:31 +0100

    el war no esta muya alla, me sale error en los test pero no en jupyter

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,97 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        attack = self.strength 
+        return self.strength
+    def receivedDamage(self,receivedDamage = int(input(f'introduce el daño recibido ----> '))):
+            self.health -= receivedDamage 
+sol1 = Soldier(500,250)
 
+
+sol1
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+      def __init__(self,name, health, strength):
+        super().__init__( health, strength)
+        self.name = name
+        self.health = health
+        self.strength = strength
+      
+      def receivedDamage(self):
+            receivedDamage = int(input(f'introduce el daño recibido ----> '))
+            self.health -= receivedDamage
+            if self.health > 0:
+                 return f'{self.name} has received {self.receivedDamage} points of damage'
+                
+            else:
+                 return f'{self.name} has died in act of combat'    
+                
+      
+      def battleCry(self):
+            return f'Odin Owns You All!'
+
+
+
+
+
+
+
 
 # Saxon
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+        self.health = health
+        self.strength = strength
+    def receivedDamage(self):
+            receivedDamage = int(input(f'introduce el daño recibido ----> '))
+            self.health -= receivedDamage 
+            if self.health > 0:
+                 return f'Saxon has received {self.receivedDamage} points of damage'
+                
+            else:
+                 return f'Saxon has died in act of combat'    
 
 
-class Saxon:
-    pass
 
 # War
-
-
 class War:
-    pass
+    vikingArmy =[]
+    saxonArmy = []
+    
+    def addViking(Viking):
+        vik = Viking()
+        vikingArmy.add(vik) 
+    
+    def addSaxon(Saxon):
+        sax = Saxon()
+        saxonArmy.add(sax)
+        
+    def vikingAttack(Viking):
+           def receivedDamage(self):
+            receivedDamage = Saxon.strength
+            self.health -= receivedDamage
+            if self.health > 0:
+                 return f'{self.name} has received {self.receivedDamage} points of damage'
+                
+            else:
+                 return f'{self.name} has died in act of combat' 
+    
+    
+    def saxonAttack(Saxon):
+        def receivedDamage(self):
+            receivedDamage = Viking.strenght
+            self.health -= receivedDamage 
+            if self.health > 0:
+                 return f'Saxon has received {self.receivedDamage} points of damage'
+                
+            else:
+                 return f'Saxon has died in act of combat'    
+    def showStatus:
