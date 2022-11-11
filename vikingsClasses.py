eef770ce312254fc58267f00dbb179743fd65c6b
annassanchez <annassanchez@gmail.com>
2022-11-11 18:33:34 +0100

    (o゜▽゜)o☆ 4 clases definidas, a por el bonus

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -50,36 +50,40 @@ class War:
     def __init__(self):
         self.vikingArmy = []
         self.saxonArmy = []
-
+    ##metodo01: addViking, añade elementos de la subclase Vikings a mi éjercito
     def addViking(self, viking):
         self.vikingArmy.append(viking)
-
+    ##metodo02: addSaxon, añade elementos de la subclase Saxons a mi éjercito
     def addSaxon(self, saxon):
         self.saxonArmy.append(saxon)
-    
+    ##metodo03: ataqueVikingo, la salud del atacado se resta según la fuerza del oponente
+    ##si muere, me lo quito del ejército
     def vikingAttack(self):
         import random ## para seleccionar a un muembro random de cada ejército
         random_saxon = random.choice(self.saxonArmy)
         random_viking = random.choice(self.vikingArmy)
-        random_saxon.receiveDamage(random_viking.strength)
+        resultado = random_saxon.receiveDamage(random_viking.strength) ##ojo, si lo hago igual a receivDamage() ya me da todos los valores del daño que he definido antes
         if random_saxon.health <= 0:
             self.saxonArmy.remove(random_saxon)
-        return random_saxon.receiveDamage(random_viking.strength)
-    
+            #return f"A Saxon has died in combat"
+        return resultado
+    ##metodo04: ataqueVikingo, la salud del atacado se resta según la fuerza del oponente
+    ##si muere, me lo quito del ejército
     def saxonAttack(self):
         import random ## para seleccionar a un muembro random de cada ejército
         random_saxon = random.choice(self.saxonArmy)
         random_viking = random.choice(self.vikingArmy)
-        random_viking.receiveDamage(random_saxon.strength)
+        resultado = random_viking.receiveDamage(random_saxon.strength)
         if random_viking.health <= 0:
             self.vikingArmy.remove(random_viking)
-        return random_viking.receiveDamage(random_saxon.strength)
-    
+            #return f"{random_viking.name} has died in act of combat"
+        return resultado
+    ##metodo05: checkea si quedan miembros en las listas éjercitos, sino ya acaba
     def showStatus(self):
         if len(self.vikingArmy) == 0:
             return f"Saxons have fought for their lives and survive another day..."
         elif len(self.saxonArmy) == 0:
             return f"Vikings have won the war of the century!"
-        elif len(self.vikingArmy)  >0 and len(self.vikingArmy) > 0:
+        elif len(self.saxonArmy)  >0 and len(self.vikingArmy) > 0:
             return f"Vikings and Saxons are still in the thick of battle."
 
